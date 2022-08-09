c8c7554f0f0ae954bf488c121b268f1e1e1c17a2
ABISHEKAS <abishek.sengottuvel@gmail.com>
2022-08-09 07:34:29 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import classes from './App.module.css';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className={classes.Topbar}>
+          <img src="https://images-na.ssl-images-amazon.com/images/G/01/gc/designs/livepreview/amazon_dkblue_noto_email_v2016_us-main._CB468775337_.png"/>
+        </nav>
       </header>
+      <div className={classes.MainContainer}>
+        <div className={classes.ProductPreview}>
+          <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview"/>
+        </div>
+      </div>
     </div>
   );
 }
