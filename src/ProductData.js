9ed0cdc28b241ba7652df945d1129156bf94bcc3
ABISHEKAS <abishek.sengottuvel@gmail.com>
2022-08-09 09:14:11 +0530

    Commit Changes on 9th August 2022

---
+++ src/ProductData.js
@@ -0,0 +1,23 @@
+const ProductData = {
+    title:'FitBit 19 - The Smartest Watch',
+    description:'This is a most flexible wearable digital cum analog watch used by all people on the daily potential basis.',
+    colorOptions:[
+        {
+            styleName:'Red Strap',
+            imageUrl:'https://imgur.com/PTgQlim.png'
+        },
+        {
+            styleName:'Black Strap',
+            imageUrl:'https://imgur.com/iOeUBV7.png'
+        },
+        {
+            styleName:'Blue Strap',
+            imageUrl:'https://imgur.com/Mplj1YR.png'
+        },
+        {
+            styleName:'Purple Strap',
+            imageUrl:'https://imgur.com/xSIK4M8.png'
+        }
+    ]
+}
+export default ProductData
